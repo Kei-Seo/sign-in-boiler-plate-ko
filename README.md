dc5ac463278eba9c21961f9d6ac01911bade0971
HONGSEOK SEO <hongseokseo@HONGSEOKui-MacBookPro.local>
2023-04-01 15:26:44 +0900

    add node man

---
+++ README.md
@@ -0,0 +1,1 @@
+# sign-in-boiler-plate-ko
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User'); 
+
+//application/w-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kei:1p2p3p4p@boilerplate.fgyxeqg.mongodb.net/?retryWrites=true&w=majori', {
+mongoose.connect('mongodb+srv://kei:1p2p3p4p@boilerplate.fgyxeqg.mongodb.net/?retryWrites=true', {
     useNewUrlParser: true, useUnifiedTopology: true 
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
+app.get('/', (req, res) => res.send("Hello World! ~ 안녕하세요 \n"))
 
-app.get('/', (req, res) => res.send("Hello World!\n"))
+
+app.post('/register', async(req, res) => {
+
+    const user = new User(req.body)
+    const result = await user.save().then(() => {
+        res.status(200).json({
+            success : true
+        })
+    }).catch((err) => {
+        res.json({ success : false , err})
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
